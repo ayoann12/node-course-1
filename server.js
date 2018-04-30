7663b59cd7c079ec6f8b5da16b4ebdfc55788f5d
unknown <ayoann12@yahoo.fr>
2018-04-30 12:49:58 +0100

    Add **projects** page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const request = require('request');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -17,19 +18,48 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.render('maintenance.hbs');
 });
+ */
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-hbs.registerHelper('getCurrentYear', () => {
+hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text) => {
+hbs.registerHelper("screamIt", (text) => {
   return text.toUpperCase();
 });
+
+hbs.registerHelper("projects", (userName) => {
+  const encodedUserName = encodeURIComponent(userName);
+  request({
+    url: `https://api.github.com/users/${encodedUserName}/repos`,
+    json: true,
+    headers: {
+      "User-Agent": "request"
+    }
+  }, (error, response, body) => {
+    if (error) {
+      return 'Unable to connect to Github.com server.';
+    } else if (response.statusCode === 400 || response.statusCode === 403) {
+      console.log(`Unable to fetch repos of ${userName}.`);
+      return `Unable to fetch repos of ${userName}.`;
+    } else if (response.statusCode === 200) {
+      const resultat = [];
+      body.map(element => {
+        resultat.push({
+          name: element.name,
+          full_name: element.full_name,
+          owner: element.owner.login
+        })
+      });
+      return JSON.stringify(resultat, undefined, 2);
+    }
+  });
+})
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
@@ -42,6 +72,13 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
     blablaMessage: 'Some text here, i don\'t know yet what to say...'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio Page',
+    blablaMessage: 'These are some projects i want to show'
   });
 });
 
